6c0f651f3a0b53d9b97252c1fa4bbd0646e47a8e
Gamer247 <darshanjsg2003@gmail.com>
2023-10-29 12:39:19 +0530

    changed colorscheme of dmenu

--- .local/bin/suckless/dmenu/config.h
+++ .local/bin/suckless/dmenu/config.h
@@ -9,9 +9,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	// [SchemeNorm] = { "#bbbbbb", "#222222" },
+	// [SchemeSel] = { "#eeeeee", "#005577" },
+	// [SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#B4D0E9", "#011628" },
+	[SchemeSel] = { "#CBE0F0", "#0A64AC" },
+	[SchemeOut] = { "#011628", "#275378" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
