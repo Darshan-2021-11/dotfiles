bcc1afc3070fec3f1fee16efaa213731a4b6f40c
Gamer247 <darshanjsg2003@gmail.com>
2024-01-17 14:10:34 +0530

    Updated dwm look

--- src/suckless/dwm/config.h
+++ src/suckless/dwm/config.h
@@ -1,19 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int snap      = 8;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 // static const char *fonts[]          = { "Monospace:size=10" };
 // static const char dmenufont[]       = "Monospace:size=12";
 static const char *fonts[]          = { "SauceCodePro Nerd Font:size=9" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font:size=9";
-static const char col_gray1[]       = "#011628";
-static const char col_gray2[]       = "#275378";
+static const char col_gray1[]       = "#16161E";
+static const char col_gray2[]       = "#16161E";
 static const char col_gray3[]       = "#B4D0E9";
-static const char col_gray4[]       = "#CBE0F0";
-static const char col_cyan[]        = "#0A64AC";
+static const char col_gray4[]       = "#B4D0E9";
+static const char col_cyan[]        = "#16161E";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
