9a9c9cbdad8a1f5feb8f582b8ba7eb493631e525
Gamer247 <darshanjsg2003@gmail.com>
2024-03-05 22:44:41 +0530

    updated unknown monospace font to gnu-free-font's FreeMono

--- .local/src/suckless/st/config.h
+++ .local/src/suckless/st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "SauceCodePro Nerd Font:style=Medium:pixelsize=14:antialias=true:autohint=true";
-static char *font = "monospace:style=Medium:pixelsize=18:antialias=true:autohint=true";
+static char *font = "FreeMono:style=Medium:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
